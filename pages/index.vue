0bfddefea4c322b3c77352868177caf2ec509d1f
t.urbutt <tim@urbutt.de>
2024-04-23 13:33:41 +0200

    design changes

--- pages/index.vue
+++ pages/index.vue
@@ -235,7 +235,7 @@ export default {
 };
 </script>
   
-  <style scoped>
+  <style>
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap');
 
